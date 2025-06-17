a9ac221ebb9d56198111b20022da18dce1cadefa
sangah323 <sangah@Testui-MacBookAir.local>
2025-06-17 11:35:46 +0900

    fix: 백엔드 app.ts 파일 수정

--- apps/tuner-api/src/app.ts
+++ apps/tuner-api/src/app.ts
@@ -1,9 +1,20 @@
 import express from "express";
-import { adminRoutes, authRoutes, userRoutes, surveyRoutes } from "../src/routes";
+import {
+  adminRoutes,
+  authRoutes,
+  userRoutes,
+  surveyRoutes,
+} from "../src/routes";
 import cors from "cors";
 
-
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_IP || "http://192.168.0.59:5173",
+    credentials: true,
+  })
+);
 
 // 미들웨어 설정
 app.use(express.json());
@@ -17,20 +28,20 @@ app.use("/admin", adminRoutes);
 
 // 기본 라우트
 app.get("/", (req, res) => {
-    res.json({ status: "ok", uptime: process.uptime() });
+  res.json({ status: "ok", uptime: process.uptime() });
 });
-
-app.use(
-    cors({
-        origin: process.env.CLIENT_IP || 'http://192.168.0.59:5173',
-        credentials: true,
-    }),
-);
 
 // 에러 핸들링 미들웨어
-app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+app.use(
+  (
+    err: Error,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
     console.error(err);
     res.status(500).json({ error: "Internal Server Error" });
-});
+  }
+);
 
-export default app; 
+export default app;
