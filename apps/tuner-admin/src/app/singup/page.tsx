483362d6de5e097844f0da92874c5438f5cc6561
mls <cjmadlife5930@gmail.com>
2025-06-17 15:24:55 +0900

    병합 해결

--- apps/tuner-admin/src/app/singup/page.tsx
+++ apps/tuner-admin/src/app/singup/page.tsx
@@ -3,7 +3,7 @@ import { useSessionCheck } from '@/hooks/useSessionCheck';
 import SingUpForm from './components/SingUpForm';
 
 const Signup = () => {
-    // useSessionCheck(); // 클라이언트 훅 호출
+    useSessionCheck(); // 클라이언트 훅 호출
     return <SingUpForm />;
 };
 
