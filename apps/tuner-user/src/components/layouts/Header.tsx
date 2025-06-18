bc0b8562d31c84627c631262bf28366f364a50a3
sangah323 <sangah@Testui-MacBookAir.local>
2025-06-18 11:06:20 +0900

    fix: sidebar mouseEvent 수정

--- apps/tuner-user/src/components/layouts/Header.tsx
+++ apps/tuner-user/src/components/layouts/Header.tsx
@@ -9,7 +9,7 @@ export default function Header() {
 
   return (
     <>
-      <header className="fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-[485px] min-h-[52px] flex justify-between items-center bg-white text-black border border-red-500 px-4 py-2 z-30">
+      <header className="fixed top-0 left-1/2 transform -translate-x-1/2 w-full max-w-[485px] min-h-[52px] flex justify-between items-center bg-white text-black border border-red-500 px-4 py-2">
         <div>
           <Link href="/" className="text-2xl font-bold">
             LOGO
@@ -17,7 +17,10 @@ export default function Header() {
         </div>
         <nav className="flex gap-4 items-center">
           <Link href="/search">Search</Link>
-          <div onClick={() => setSidebarOpen(true)} className="text-xl">
+          <div
+            onClick={() => setSidebarOpen(true)}
+            className="text-xl font-bold cursor-pointer"
+          >
             ☰
           </div>
         </nav>
