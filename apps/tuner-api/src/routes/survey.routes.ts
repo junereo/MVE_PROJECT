65986586626884b09245aaad8c2841d5a74c294d
junereo <taejung.joe.kim@gmail.com>
2025-06-27 14:12:44 +0900

    feat: cron 스케쥴링 구현

--- apps/tuner-api/src/routes/survey.routes.ts
+++ apps/tuner-api/src/routes/survey.routes.ts
@@ -4,12 +4,13 @@ import { verifyUserOrAdmin } from "../middlewares/survey.middleware";
 
 const router = express.Router();
 
+console.log("createSurveyHandler:", typeof createSurveyHandler);
 
 // router.get('/list');
 // router.get('/:surveyId');
 // router.get('/:surveyId/results');
 router.post('/create', verifyUserOrAdmin, createSurveyHandler);
-router.post('/:surveyId/responses');
+// router.post('/:surveyId/responses');
 // router.put('/:surveyId/responses');
 
 export default router;
