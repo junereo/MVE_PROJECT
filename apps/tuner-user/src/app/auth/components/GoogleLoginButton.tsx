e3e607d0bad0263a63bc6c02087ed260a2c6ff54
chicEUN <sjrnfl7gmail.com>
2025-07-02 14:07:13 +0900

    통합 로그인 완료

--- apps/tuner-user/src/app/auth/components/GoogleLoginButton.tsx
+++ apps/tuner-user/src/app/auth/components/GoogleLoginButton.tsx
@@ -3,7 +3,12 @@
 import Button from "@/components/ui/Button";
 
 export default function GoogleLoginButton() {
-  const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&response_type=code&scope=openid%20email%20profile`;
+  const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth` +
+    `?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}` +
+    `&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}` +
+    `&response_type=code` +
+    `&scope=openid%20email%20profile`;
+
   console.log("GOOGLE_AUTH_URL", GOOGLE_AUTH_URL);
 
 
