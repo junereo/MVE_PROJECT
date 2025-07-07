7d8472c798ec9bbff5e239b7235e1385b1af169c
junereo <taejung.joe.kim@gmail.com>
2025-07-07 21:45:47 +0900

    feat : contractRoutes 기능 추가

--- apps/tuner-api/src/wallet/routers/index.ts
+++ apps/tuner-api/src/wallet/routers/index.ts
@@ -3,7 +3,9 @@ import express, { Router } from "express";
 import walletRoutes from "./wallet.routes";
 import txpoolRoutes from "./txpool.routes";
 import surveyRoutes from "./survey.routes";
+import contractRoutes from "./contract.info.routes";
 import { verifyToken } from "../middlewares/auth.middleware";
+import { Prisma } from '@prisma/client';
 
 const router: Router = express.Router();
 
@@ -11,5 +13,18 @@ const router: Router = express.Router();
 router.use('/wallet',  walletRoutes);
 router.use('/tx',  txpoolRoutes);
 router.use('/survey',  surveyRoutes);
+router.use('/ca',  contractRoutes);
 
 export default router;
+
+export type TunerContract = {
+  id: number;
+  ca_token?: string | null;
+  ca_badge?: string | null;
+  ca_survey?: string | null;
+  ca_transac?: string | null;
+  abi_survey?: Prisma.JsonValue | null;
+  abi_transac?: Prisma.JsonValue | null;
+  created_at: Date;
+  updated_at: Date;
+};
