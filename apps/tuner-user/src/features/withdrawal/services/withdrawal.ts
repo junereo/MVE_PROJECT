0a2c1e1758949bc5890eb3e4689bda5c56c608c7
sangah323 <sangah@Testui-MacBookAir.local>
2025-07-17 15:37:36 +0900

    chore: 디버깅용 console.log 제거

--- apps/tuner-user/src/features/withdrawal/services/withdrawal.ts
+++ apps/tuner-user/src/features/withdrawal/services/withdrawal.ts
@@ -1,11 +1,4 @@
 import axios from "@/lib/network/axios";
-import { WithdrawalRequestPayload } from "../types/withdrawal";
-
-// 출금 요청
-export const requestWithdrawal = async (payload: WithdrawalRequestPayload) => {
-  const res = await axios.post("/withdraw", payload);
-  return res.data;
-};
 
 // 출금 내역
 export const getUserWithdrawals = async (userId: number) => {
